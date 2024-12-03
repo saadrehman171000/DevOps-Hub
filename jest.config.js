3e01d3f3405223a93eda8c0dc5881e9bf60b63ce
saadrehman171000 <saadrehman1710000@gmail.com>
2024-12-03 18:09:40 +0500

    Updated workflow

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,7 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/app/(.*)$': '<rootDir>/app/$1'
-  },
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-  },
-  testEnvironmentOptions: {
-    url: 'http://localhost'
+    '^@/(.*)$': '<rootDir>/src/$1'
   }
 } 
