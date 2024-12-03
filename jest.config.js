b8ceacc44f412c0d727e828b43fad948035e7494
saadrehman171000 <saadrehman1710000@gmail.com>
2024-12-03 18:55:39 +0500

    Updated packages

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,20 @@
-module.exports = {
-  testEnvironment: 'jsdom',
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js'
-  },
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest']
-  },
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)']
-} 
+    // Handle module aliases
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/app/(.*)$': '<rootDir>/src/app/$1'
+  }
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig) 
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,13 @@
-require('@testing-library/jest-dom'); 
+import '@testing-library/jest-dom'
+
+// Mock the Kinde Auth module
+jest.mock('@kinde-oss/kinde-auth-nextjs', () => ({
+  useKindeBrowserClient: jest.fn().mockReturnValue({
+    isLoading: false,
+    isAuthenticated: false,
+    user: null,
+    login: jest.fn(),
+    register: jest.fn(),
+    logout: jest.fn()
+  })
+})) 
