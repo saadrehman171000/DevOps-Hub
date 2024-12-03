653f699a17f762f41f3d0bf8f6d6e8eb6101a2cb
saadrehman171000 <saadrehman1710000@gmail.com>
2024-12-03 18:18:43 +0500

    Updated packages

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  }
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/']
 } 
