0c3a2e4128edd4d3cf0a41256efdb1f45ad8c646
saadrehman171000 <saadrehman1710000@gmail.com>
2024-12-03 19:58:33 +0500

    updated utils.ts

--- jest.config.js
+++ jest.config.js
@@ -12,12 +12,14 @@ const customJestConfig = {
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
     '^@/app/(.*)$': '<rootDir>/src/app/$1',
     '^../../components/(.*)$': '<rootDir>/src/components/$1',
+    // Add this line to handle lib imports
+    '^../../../lib/(.*)$': '<rootDir>/src/lib/$1',
     // Handle CSS imports (with CSS modules)
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     // Handle CSS imports (without CSS modules)
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
     // Handle image imports
-    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/__mocks__/fileMock.js'
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': 'test-file-stub'
   },
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
