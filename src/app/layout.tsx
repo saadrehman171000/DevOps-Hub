985be9f41914dfec7406566bcc520cf945d773bd
zaiffishiekh01 <zaiffishiekh@gmail.com>
2024-12-10 23:12:36 +0500

    Refactor RootLayout component for clarity and consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,10 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { redirect } from 'next/navigation'
-import { Toaster } from "sonner";
+import { Toaster } from "sonner"
 
 const inter = Inter({ subsets: ['latin'] })
-  
+
 export const metadata: Metadata = {
   title: 'DevOpsHub',
   description: 'Your one-stop resource for DevOps tools and best practices',
@@ -20,9 +20,11 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  // Get authentication session status from Kinde
   const { isAuthenticated } = getKindeServerSession()
   const authStatus = await isAuthenticated()
 
+  // Redirect to login page if user is not authenticated
   if (!authStatus) {
     redirect('/api/auth/login')
   }
